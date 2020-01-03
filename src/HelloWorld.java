56c0b2904f000a6024841fc639946d65b89ac2d1
mcheich <mcheich@gmail.com>
2020-01-03 13:39:54 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author michaelJames
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Worlds!");
+
+	}
+
+}
+
